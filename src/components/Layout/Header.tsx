1340e069f608b4544a5a27c4c5d5c4f0b3d13562
Jaume Alavedra <jamalavedra@gmail.com>
2024-04-04 15:58:15 +0200

    add source

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,14 @@
 export default function Header(props: any) {
   return (
     <div className="bg-gray-200 text-zinc-700 text-center py-4">
-      Self-Custodial Smart Account Demo
+      SELF-CUSTODIAL SMART ACCOUNT
+      <a
+        href="https://github.com/openfort-xyz/sample-browser-nextjs-embedded-signer"
+        target="_blank"
+        className="text-blue-500"
+      >
+        (View Source)
+      </a>
     </div>
   );
 }
