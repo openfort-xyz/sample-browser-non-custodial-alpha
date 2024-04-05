0a6a07d6a515b3aa147f17f72fef777afa70576d
gllm-dev <guillem@openfort.xyz>
2024-04-05 18:15:15 +0200

    feat: project entropy

--- src/utils/openfortConfig.ts
+++ src/utils/openfortConfig.ts
@@ -4,11 +4,12 @@ import Openfort from '@openfort/openfort-js';
 const openfortConfig = {
   publicKey: process.env.NEXT_PUBLIC_OPENFORT_PUBLIC_KEY!,
   shieldApiKey: process.env.NEXT_PUBLIC_SHIELD_API_KEY,
+  shieldEncryptionShare: process.env.NEXT_PUBLIC_SHIELD_ENCRYPTION_SHARE,
   // Add any other configuration parameters required by your Openfort setup
 };
 
 // Initialize the Openfort SDK
-const openfort = new Openfort(openfortConfig.publicKey, openfortConfig.shieldApiKey);
+const openfort = new Openfort(openfortConfig.publicKey, openfortConfig.shieldApiKey, openfortConfig.shieldEncryptionShare);
 
 // Optionally, configure additional Openfort settings or initialize more services here
 
