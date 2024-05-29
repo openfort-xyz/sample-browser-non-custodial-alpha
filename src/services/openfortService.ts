f0bef51711564e2ac96bfb4faa5d5c2ca7c964c7
Jaume Alavedra <jamalavedra@gmail.com>
2024-05-29 11:07:01 +0200

    fix: upgrade to new reference

--- src/services/openfortService.ts
+++ src/services/openfortService.ts
@@ -6,7 +6,7 @@ const chainId = 80002;
 class OpenfortService {
     async authenticateWithThirdPartyProvider(identityToken: string): Promise<AuthPlayerResponse> {
       try {
-        return await openfort.authenticateWithThirdPartyProvider(OAuthProvider.Firebase, identityToken, TokenType.IdToken);
+        return await openfort.authenticateWithThirdPartyProvider(OAuthProvider.FIREBASE, identityToken, TokenType.ID_TOKEN);
       } catch (error) {
         console.error('Error authenticating with Openfort:', error);
         throw error;
@@ -89,7 +89,7 @@ class OpenfortService {
           authProvider: "firebase",
           tokenType: "idToken",
         };
-        await openfort.configureEmbeddedSignerRecovery(chainId, shieldAuth, pin);
+        await openfort.configureEmbeddedSigner(chainId, shieldAuth, pin);
       } catch (error) {
         console.error('Error authenticating with Openfort:', error);
         throw error;
--- src/utils/openfortConfig.ts
+++ src/utils/openfortConfig.ts
@@ -1,12 +1,14 @@
 import Openfort from '@openfort/openfort-js';
 
-const openfortConfig = {
-  publicKey: process.env.NEXT_PUBLIC_OPENFORT_PUBLIC_KEY!,
-  shieldApiKey: process.env.NEXT_PUBLIC_SHIELD_API_KEY,
-  shieldEncryptionShare: process.env.NEXT_PUBLIC_SHIELD_ENCRYPTION_SHARE,
-};
-
 // Initialize the Openfort SDK
-const openfort = new Openfort(openfortConfig.publicKey, openfortConfig.shieldApiKey, openfortConfig.shieldEncryptionShare);
+const openfort = new Openfort({
+  baseConfiguration: {
+    publishableKey: process.env.NEXT_PUBLIC_OPENFORT_PUBLIC_KEY!,
+  },
+  shieldConfiguration: {
+    shieldPublishableKey: process.env.NEXT_PUBLIC_SHIELD_API_KEY,
+    shieldEncryptionKey: process.env.NEXT_PUBLIC_SHIELD_ENCRYPTION_SHARE,
+  },
+});
 
 export default openfort;
